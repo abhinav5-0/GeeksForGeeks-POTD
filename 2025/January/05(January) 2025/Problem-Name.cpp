0512c1657f79c118b7856d2a0cddcd78cd18625e
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-01-05 14:42:59 +0530

    Update Problem-Name.cpp

---
+++ 2025/January/05(January) 2025/Problem-Name.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPairs(vector<int> &arr, int target) {
+        sort(arr.begin(), arr.end());
+        int count = 0;
+        int left = 0, right = arr.size() - 1;
+        
+        while (left < right) {
+            if (arr[left] + arr[right] < target) {
+                count += (right - left);
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return count;
+    }
+};
