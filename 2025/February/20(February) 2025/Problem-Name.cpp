01356830d51a69b2a1284b1c0607e4e2f714517b
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-02-20 18:06:27 +0530

    Update Problem-Name.cpp

--- 2025/February/20(February) 2025/Problem-Name.cpp
+++ 2025/February/20(February) 2025/Problem-Name.cpp
@@ -1,35 +1,34 @@
 class Solution {
-  public:
-    struct Compare {
-        bool operator()(Node* a, Node* b) {
-            return a->data > b->data;  // Min-heap based on node values
-        }
-    };
-    
-    Node* mergeKLists(vector<Node*>& arr) {
-        priority_queue<Node*, vector<Node*>, Compare> pq;
-        
-        // Push the first node of each list into the heap
-        for (Node* list : arr) {
-            if (list) pq.push(list);
-        }
-        
-        Node* dummy = new Node(0);
-        Node* tail = dummy;
-        
-        while (!pq.empty()) {
-            Node* minNode = pq.top();
-            pq.pop();
-            
-            tail->next = minNode;
-            tail = tail->next;
-            
-            if (minNode->next) {
-                pq.push(minNode->next);
+public:
+    vector<double> getMedian(vector<int> &arr) {
+        vector<double> result;
+        priority_queue<int> maxHeap; // Max heap for the smaller half
+        priority_queue<int, vector<int>, greater<int>> minHeap; // Min heap for the larger half
+
+        for (int num : arr) {
+            // Step 1: Insert into one of the heaps
+            if (maxHeap.empty() || num <= maxHeap.top()) {
+                maxHeap.push(num);
+            } else {
+                minHeap.push(num);
+            }
+
+            // Step 2: Balance the heaps
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+            } else if (minHeap.size() > maxHeap.size()) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            }
+
+            // Step 3: Calculate median
+            if (maxHeap.size() == minHeap.size()) {
+                result.push_back((maxHeap.top() + minHeap.top()) / 2.0);
+            } else {
+                result.push_back(maxHeap.top());
             }
         }
-        
-        return dummy->next;
+        return result;
     }
 };
-
