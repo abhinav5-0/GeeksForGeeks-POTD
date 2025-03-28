dca20c8d41c28589a95fb829d72feea39e8c352a
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-03-28 19:48:26 +0530

    Update Problem-Name.cpp

--- 2025/March/28(March) 2025/Problem-Name.cpp
+++ 2025/March/28(March) 2025/Problem-Name.cpp
@@ -1,28 +1,16 @@
 class Solution {
   public:
     int activitySelection(vector<int> &start, vector<int> &finish) {
-        int n = start.size();
         vector<pair<int, int>> activities;
-        
-        // Store activities as (finish time, start time) pairs
-        for (int i = 0; i < n; i++) {
-            activities.push_back({finish[i], start[i]});
-        }
-
-        // Sort activities by finish time
+        for (int i = 0; i < start.size(); i++) activities.push_back({finish[i], start[i]});
         sort(activities.begin(), activities.end());
-
-        int count = 1;  // First activity is always selected
-        int last_finish = activities[0].first;
-
-        // Select activities that do not overlap
-        for (int i = 1; i < n; i++) {
-            if (activities[i].second >= last_finish) { 
+        int count = 1, end = activities[0].first;
+        for (int i = 1; i < activities.size(); i++) {
+            if (activities[i].second > end) {
                 count++;
-                last_finish = activities[i].first; // Update last selected finish time
+                end = activities[i].first;
             }
         }
-
         return count;
     }
 };
