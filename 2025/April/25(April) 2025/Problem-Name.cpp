cc30fb8c681bba56e2b48c41d7ea292dc0b4be12
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-04-25 09:03:50 +0530

    Update Problem-Name.cpp

---
+++ 2025/April/25(April) 2025/Problem-Name.cpp
@@ -0,0 +1,30 @@
+int count = 0;
+        int candidate = -1;
+
+        // Step 1: Find potential candidate
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Step 2: Verify candidate
+        count = 0;
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        if (count > arr.size() / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
+};
