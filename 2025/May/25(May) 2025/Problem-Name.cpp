6ae45692ed59383cffac69d4c9da1fed2a88577a
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-05-25 09:40:08 +0530

    Update Problem-Name.cpp

--- 2025/May/25(May) 2025/Problem-Name.cpp
+++ 2025/May/25(May) 2025/Problem-Name.cpp
@@ -1,36 +1,17 @@
-
 class Solution {
   public:
     bool pythagoreanTriplet(vector<int>& arr) {
-        int n = arr.size();
-        vector<long long> squared(n);
-
-        // Step 1: Square all elements
-        for (int i = 0; i < n; ++i) {
-            squared[i] = 1LL * arr[i] * arr[i];
+        // code by   @ashutosh_7_Kumar
+         set<int> sets;
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] <= 0) continue;
+            sets.insert(arr[i] * arr[i]);
         }
-
-        // Step 2: Sort the squared array
-        sort(squared.begin(), squared.end());
-
-        // Step 3: Fix one element as c^2 (from end to start)
-        for (int i = n - 1; i >= 2; --i) {
-            int left = 0;
-            int right = i - 1;
-
-            // Use two pointers to find a^2 + b^2 = c^2
-            while (left < right) {
-                long long sum = squared[left] + squared[right];
-                if (sum == squared[i]) {
-                    return true;  // Triplet found
-                } else if (sum < squared[i]) {
-                    ++left;
-                } else {
-                    --right;
-                }
+        for(int a : sets) {
+            for(int b : sets) {
+                if(sets.find(a + b) != sets.end()) return true;
             }
         }
-
-        return false;  // No triplet found
+        return false;
     }
 };
