f4f710265b6b9771ebbf51ff3439535c0ae5ddbe
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-08-07 21:18:25 +0530

    Update Problem-Name.cpp

---
+++ 2025/August/07(August) 2025/Problem-Name.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int timeToSeconds(const string &time) {
+        int h = stoi(time.substr(0, 2));
+        int m = stoi(time.substr(3, 2));
+        int s = stoi(time.substr(6, 2));
+        return h * 3600 + m * 60 + s;
+    }
+
+    int minDifference(vector<string> &arr) {
+        vector<int> seconds;
+        
+        for (const string &time : arr) {
+            seconds.push_back(timeToSeconds(time));
+        }
+        
+        sort(seconds.begin(), seconds.end());
+        
+        int minDiff = INT_MAX;
+        int n = seconds.size();
+        
+        for (int i = 1; i < n; ++i) {
+            minDiff = min(minDiff, seconds[i] - seconds[i - 1]);
+        }
+        
+        // Check the wrap-around case (last and first across midnight)
+        int wrapAroundDiff = 86400 - seconds[n - 1] + seconds[0];
+        minDiff = min(minDiff, wrapAroundDiff);
+        
+        return minDiff;
+    }
+};
