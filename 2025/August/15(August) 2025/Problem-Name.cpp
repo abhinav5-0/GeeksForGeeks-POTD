8306b843a3a819db46e4af506faaa0077084877d
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-08-15 18:20:43 +0530

    Update Problem-Name.cpp

---
+++ 2025/August/15(August) 2025/Problem-Name.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    vector<vector<int>> insertInterval(vector<vector<int>> &intervals,
+                                       vector<int> &newInterval) {
+        vector<vector<int>> res;
+        int n = intervals.size();
+        int i = 0;
+
+        // Step 1: Add all intervals before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
+        // Step 2: Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.push_back(newInterval);
+
+        // Step 3: Add remaining intervals
+        while (i < n) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
+        return res;
+    }
+};
