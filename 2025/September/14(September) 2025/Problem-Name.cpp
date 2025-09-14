cd884e364fb58f93a7ccf43dbe929eb6633d29be
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-09-14 20:14:12 +0530

    Update Problem-Name.cpp

---
+++ 2025/September/14(September) 2025/Problem-Name.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        int n = gas.size();
+        int total = 0, tank = 0, start = 0;
+        
+        for(int i = 0; i < n; i++) {
+            total += gas[i] - cost[i];
+            tank += gas[i] - cost[i];
+            
+            if(tank < 0) {
+                // Can't reach this point, reset starting station
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        
+        return (total >= 0) ? start : -1;
+    }
+};
