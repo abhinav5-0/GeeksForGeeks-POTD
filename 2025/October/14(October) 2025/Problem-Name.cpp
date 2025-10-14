c6fbe64502e179ae2235c529dcad24e89fd644a1
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-10-14 20:04:32 +0530

    Update Problem-Name.cpp

--- 2025/October/14(October) 2025/Problem-Name.cpp
+++ 2025/October/14(October) 2025/Problem-Name.cpp
@@ -1,16 +1,19 @@
 class Solution {
-public:
-    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-        int n = nums.size(), inc = 1, prevInc = 0, maxLen = 0;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > nums[i - 1]) inc++;
-            else {
-                prevInc = inc;
-                inc = 1;
-            }
-            maxLen = max(maxLen, max(inc >> 1, min(prevInc, inc)));
-            if (maxLen >= k) return true;
-        }
-        return false;
+  public:
+    int nodeSum(Node* root, int l, int r) {
+        if (root == NULL)
+            return 0;
+
+        // If node value lies within [l, r]
+        if (root->data >= l && root->data <= r)
+            return root->data + nodeSum(root->left, l, r) + nodeSum(root->right, l, r);
+
+        // If node value is greater than r, go to left subtree
+        else if (root->data > r)
+            return nodeSum(root->left, l, r);
+
+        // If node value is smaller than l, go to right subtree
+        else
+            return nodeSum(root->right, l, r);
     }
 };
