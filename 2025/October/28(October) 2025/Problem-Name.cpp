a544408818c12f9382dfeca9967b74d322b44170
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-10-28 19:48:57 +0530

    Update Problem-Name.cpp

---
+++ 2025/October/28(October) 2025/Problem-Name.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<int>> nearest(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, -1));
+        queue<pair<int, int>> q;
+        
+        // Push all cells with 1 into the queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+        
+        // Directions (up, down, left, right)
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+        
+        // BFS traversal
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && dist[nr][nc] == -1) {
+                    dist[nr][nc] = dist[r][c] + 1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+        
+        return dist;
+    }
+}; // ✅ Make sure this closing brace is present!
