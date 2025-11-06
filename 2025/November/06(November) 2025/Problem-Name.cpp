1b16939cce69137ed2f94a0b94f6307b66c26e7d
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-11-06 20:18:01 +0530

    Update Problem-Name.cpp

---
+++ 2025/November/06(November) 2025/Problem-Name.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfWays(int n) {
+        if (n <= 2)
+            return n;
+        
+        int prev2 = 1, prev1 = 2, curr;
+        for (int i = 3; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
