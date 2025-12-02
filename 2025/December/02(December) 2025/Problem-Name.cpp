a094343268e71e361c99c35ce39439cf7f6bb02f
abhinav5-0 <133515688+abhinav5-0@users.noreply.github.com>
2025-12-02 19:44:45 +0530

    Update Problem-Name.cpp

--- 2025/December/02(December) 2025/Problem-Name.cpp
+++ 2025/December/02(December) 2025/Problem-Name.cpp
@@ -2,36 +2,24 @@ class Solution {
 public:
     int maxScore(string &s, vector<vector<char>> &jumps) {
         int n = s.size();
-        
-        // Map char -> list of indices
-        vector<vector<int>> pos(256);
-        for (int i = 0; i < n; i++)
-            pos[(unsigned char)s[i]].push_back(i);
-        
-        // Build allowed jumps
-        vector<vector<bool>> allow(256, vector<bool>(256, false));
-        for (auto &v : jumps)
-            allow[(unsigned char)v[0]][(unsigned char)v[1]] = true;
-        
-        // Same char always allowed
-        for (int c = 0; c < 256; c++)
-            allow[c][c] = true;
-
-        // Prefix sums of ascii
-        vector<long long> pref(n + 1, 0);
-        for (int i = 0; i < n; i++)
-            pref[i + 1] = pref[i] + (int)s[i];
-
-        // Prefix char sums
-        vector<vector<long long>> pc(256, vector<long long>(n + 1, 0));
-        for (int c = 0; c < 256; c++) {
-            for (int i = 0; i < n; i++) {
-                pc[c][i + 1] = pc[c][i] + (s[i] == c ? (int)c : 0);
+        for (char c = 'a'; c <= 'z'; c++) jumps.push_back({c, c});
+        vector<vector<int>> nxt(n, vector<int>(26, -1));
+        vector<int> last(26, -1);
+        for (int i = n - 1; i >= 0; i--) {
+            nxt[i] = last;
+            last[s[i] - 'a'] = i;
+        }
+        vector<vector<int>> ch(26);
+        for (auto &j : jumps) ch[j[0] - 'a'].push_back(j[1]);
+        vector<int> pre(n + 1);
+        for (int i = 0; i < n; i++) pre[i + 1] = pre[i] + s[i];
+        vector<int> dp(n);
+        for (int i = n - 2; i >= 0; i--) {
+            for (int c : ch[s[i] - 'a']) {
+                int j = nxt[i][c - 'a'];
+                if (j != -1) dp[i] = max(dp[i], pre[j] - pre[i + (c == s[i])] + dp[j]);
             }
         }
-
-        auto getScore = [&](int i, int j) -> long long {
-            char c = s[j];
-            long long total = pref[j] - pref[i];
-            long long sub = pc[(unsigned char)c][j] - pc[(unsigned char)c][i];
-            return total - sub;
+        return dp[0];
+    }
+};
