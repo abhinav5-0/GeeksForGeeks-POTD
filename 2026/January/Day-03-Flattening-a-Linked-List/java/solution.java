baf79207874f713ce50bee6039c15d8af4a936d3
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-01-03 22:00:44 +0530

    Add Java solution for Day-03 POTD

---
+++ 2026/January/Day-03-Flattening-a-Linked-List/java/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    // Merge two sorted bottom-linked lists
+    Node merge(Node a, Node b) {
+        if (a == null) return b;
+        if (b == null) return a;
+
+        Node result;
+
+        if (a.data < b.data) {
+            result = a;
+            result.bottom = merge(a.bottom, b);
+        } else {
+            result = b;
+            result.bottom = merge(a, b.bottom);
+        }
+
+        result.next = null; // important
+        return result;
+    }
+
+    Node flatten(Node root) {
+        // Base case
+        if (root == null || root.next == null)
+            return root;
+
+        // Flatten the right list
+        root.next = flatten(root.next);
+
+        // Merge current list with flattened list
+        root = merge(root, root.next);
+
+        return root;
+    }
+}
