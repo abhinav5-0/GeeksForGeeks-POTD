257cb1e43dd28ee59126798fd5a042cb1596f33a
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-01-09 11:53:30 +0530

    Add Python solution for Day-09 POTD

---
+++ 2026/January/Day-09-Subarrays With At Most K Distinct Integers/Python Solution/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countAtMostK(self, arr, k):
+        freq = {}
+        left = 0
+        ans = 0
+
+        for right in range(len(arr)):
+            freq[arr[right]] = freq.get(arr[right], 0) + 1
+
+            while len(freq) > k:
+                freq[arr[left]] -= 1
+                if freq[arr[left]] == 0:
+                    del freq[arr[left]]
+                left += 1
+
+            ans += (right - left + 1)
+
+        return ans
