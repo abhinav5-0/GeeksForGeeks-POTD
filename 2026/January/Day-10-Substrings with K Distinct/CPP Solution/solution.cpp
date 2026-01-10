c0a0a9e7b1fedfe472551fe0f1177878185b18a2
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-01-10 21:32:07 +0530

    Add C++ solution for Day-10 POTD

---
+++ 2026/January/Day-10-Substrings with K Distinct/CPP Solution/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long atMost(string &s, int k) {
+        if (k < 0) return 0;
+
+        vector<int> freq(26, 0);
+        int left = 0, distinct = 0;
+        long long ans = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            if (freq[s[right] - 'a'] == 0)
+                distinct++;
+            freq[s[right] - 'a']++;
+
+            while (distinct > k) {
+                freq[s[left] - 'a']--;
+                if (freq[s[left] - 'a'] == 0)
+                    distinct--;
+                left++;
+            }
+
+            ans += (right - left + 1);
+        }
+        return ans;
+    }
+
+    int countSubstr(string& s, int k) {
+        return atMost(s, k) - atMost(s, k - 1);
+    }
+};
