e75a122460371b1a8e26d1c3a3588e894acceac3
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-01-14 20:34:11 +0530

    Add C++ solution for Day-14 POTD

---
+++ 2026/January/Day-14-Police and Thieves/CPP Solution/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int catchThieves(vector<char> &arr, int k) {
+vector<int> police, thief;
+for (int i = 0; i < arr.size(); i++) {
+if (arr[i] == 'P') police.push_back(i);
+else thief.push_back(i);
+}
+int i = 0, j = 0, count = 0;
+while (i < police.size() && j < thief.size()) {
+if (abs(police[i] - thief[j]) <= k) {
+count++;
+i++;
+j++;
+}
+else if (police[i] < thief[j]) {
+i++;
+}
+else {
+j++;
+}
+}
+return count;
+}
+};
