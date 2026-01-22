3fedaa8c584632107c652473803e7dd203e98785
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-01-22 22:18:21 +0530

    Add C++ solution for Day-22 POTD

---
+++ 2026/January/Day-22-Sum of subarray ranges/CPP solution/solution.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int subarrayRanges(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> pge(n), nge(n), pse(n), nse(n);
+        stack<int> st;
+
+        // Previous Greater Element
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] <= arr[i]) st.pop();
+            pge[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+
+        // Next Greater Element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] < arr[i]) st.pop();
+            nge[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+
+        // Previous Smaller Element
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) st.pop();
+            pse[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+
+        // Next Smaller Element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] > arr[i]) st.pop();
+            nse[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        long long maxSum = 0, minSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            long long maxCnt = (long long)(i - pge[i]) * (nge[i] - i);
+            long long minCnt = (long long)(i - pse[i]) * (nse[i] - i);
+            maxSum += arr[i] * maxCnt;
+            minSum += arr[i] * minCnt;
+        }
+
+        return (int)(maxSum - minSum);
+    }
+};
