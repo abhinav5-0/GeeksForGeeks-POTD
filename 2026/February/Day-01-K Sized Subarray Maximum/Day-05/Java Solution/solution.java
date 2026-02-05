6ebd8f577c018c8f6b0d893739f923163f243e42
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-02-05 11:21:37 +0530

    Add Java solution for Day-05 POTD

---
+++ 2026/February/Day-01-K Sized Subarray Maximum/Day-05/Java Solution/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxOnes(int[] arr, int k) {
+        int left = 0, zeroCount = 0, ans = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] == 0)
+                zeroCount++;
+
+            while (zeroCount > k) {
+                if (arr[left] == 0)
+                    zeroCount--;
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+}
