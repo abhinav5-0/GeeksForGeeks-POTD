fc2f28c04ed9fee4fd4cd5500ec361a88b3d5e48
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-02-17 22:47:54 +0530

    Add Python solution for Day-17 POTD

---
+++ 2026/February/Day-01-K Sized Subarray Maximum/Day-17/python solution/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def overlapInt(self, arr):
+        n = len(arr)
+        
+        start = []
+        end = []
+        
+        for s, e in arr:
+            start.append(s)
+            end.append(e)
+        
+        start.sort()
+        end.sort()
+        
+        i = j = 0
+        curr_overlap = 0
+        max_overlap = 0
+        
+        while i < n and j < n:
+            if start[i] <= end[j]:
+                curr_overlap += 1
+                max_overlap = max(max_overlap, curr_overlap)
+                i += 1
+            else:
+                curr_overlap -= 1
+                j += 1
+        
+        return max_overlap
