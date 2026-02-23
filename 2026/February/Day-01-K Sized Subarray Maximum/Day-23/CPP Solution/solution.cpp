896ea4be15db85a86c1272d593a475785d02228e
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-02-23 12:45:05 +0530

    Add C++ solution for Day-23 POTD

---
+++ 2026/February/Day-01-K Sized Subarray Maximum/Day-23/CPP Solution/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        set<int> s;
+        
+        // Insert elements of array a
+        for(int i = 0; i < a.size(); i++) {
+            s.insert(a[i]);
+        }
+        
+        // Insert elements of array b
+        for(int i = 0; i < b.size(); i++) {
+            s.insert(b[i]);
+        }
+        
+        // Store result in vector
+        vector<int> result(s.begin(), s.end());
+        return result;
+    }
+};
