bb288c5b0a95254178db26a172650936fa551cc8
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-03-14 22:35:15 +0530

    Create solution.java

---
+++ 2026/March /Day-14/Java /solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    static class Pair {
+        Node node;
+        int hd;
+
+        Pair(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    static ArrayList<Integer> topView(Node root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(root, 0));
+
+        while(!q.isEmpty()) {
+            Pair p = q.poll();
+            Node node = p.node;
+            int hd = p.hd;
+
+            if(!map.containsKey(hd))
+                map.put(hd, node.data);
+
+            if(node.left != null)
+                q.add(new Pair(node.left, hd - 1));
+
+            if(node.right != null)
+                q.add(new Pair(node.right, hd + 1));
+        }
+
+        for(int val : map.values())
+            result.add(val);
+
+        return result;
+    }
+}
