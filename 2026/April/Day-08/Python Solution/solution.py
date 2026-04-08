f98249c3bbaa71e61ea11c2c0893a48ebbab4c35
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-04-08 20:10:19 +0530

    Create solution.py

---
+++ 2026/April/Day-08/Python Solution/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def segregate0and1(self, arr):
+        left, right = 0, len(arr) - 1
+
+        while left < right:
+            if arr[left] == 0:
+                left += 1
+            elif arr[right] == 1:
+                right -= 1
+            else:
+                arr[left], arr[right] = arr[right], arr[left]
+                left += 1
+                right -= 1
