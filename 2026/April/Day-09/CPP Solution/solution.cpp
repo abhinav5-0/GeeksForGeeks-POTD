11a16519bdecfc1fb819931b21b6e9bbba282c33
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-04-09 19:35:49 +0530

    Create solution.cpp

---
+++ 2026/April/Day-09/CPP Solution/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+        int i = 0, j = 0;
+        vector<int> res;
+
+        while (i < a.size() && j < b.size()) {
+            if (a[i] == b[j]) {
+                if (res.empty() || res.back() != a[i])
+                    res.push_back(a[i]);
+                i++;
+                j++;
+            }
+            else if (a[i] < b[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+
+        return res;
+    }
+};
