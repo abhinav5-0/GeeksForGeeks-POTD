7d00ff2bb835691578c16641b765ddfd8f47d623
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-04-14 23:08:24 +0530

    Create solution.cpp

---
+++ 2026/April/Day-14/CPP Solution/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string removeSpaces(string& s) {
+        string result = "";
+        for(char c : s) {
+            if(c != ' ') {
+                result += c;
+            }
+        }
+        return result;
+    }
+};
