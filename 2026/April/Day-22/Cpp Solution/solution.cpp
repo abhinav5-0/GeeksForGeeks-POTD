60ba86cc9a359a9f18db272d53ee7b8f82df7f7e
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-04-21 23:51:42 +0530

    Create solution.cpp

---
+++ 2026/April/Day-22/Cpp Solution/solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+
+    int pour(int from, int to, int d) {
+        int fromJug = from;
+        int toJug = 0;
+        int steps = 1;
+
+        while (fromJug != d && toJug != d) {
+            int temp = min(fromJug, to - toJug);
+
+            toJug += temp;
+            fromJug -= temp;
+            steps++;
+
+            if (fromJug == d || toJug == d)
+                break;
+
+            if (fromJug == 0) {
+                fromJug = from;
+                steps++;
+            }
+
+            if (toJug == to) {
+                toJug = 0;
+                steps++;
+            }
+        }
+        return steps;
+    }
+
+    int minSteps(int m, int n, int d) {
+        if (d > max(m, n)) return -1;
+        if (d % gcd(m, n) != 0) return -1;
+
+        return min(pour(m, n, d), pour(n, m, d));
+    }
+};
