fd136e30d9c5e3e50371471bac8843ae5ecf65fe
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-04-27 23:21:41 +0530

    Create solution.java

---
+++ 2026/April/Day-27/Java Solution/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int smallestSubstring(String s) {
+        int[] count = new int[3];
+        int left = 0;
+        int ans = Integer.MAX_VALUE;
+
+        for (int right = 0; right < s.length(); right++) {
+            count[s.charAt(right) - '0']++;
+
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                ans = Math.min(ans, right - left + 1);
+                count[s.charAt(left) - '0']--;
+                left++;
+            }
+        }
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
