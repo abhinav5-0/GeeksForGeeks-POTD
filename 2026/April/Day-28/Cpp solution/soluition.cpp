b4d83521a33d84c3ce8982989493e8e371c887e7
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-04-28 23:43:00 +0530

    Create soluition.cpp

---
+++ 2026/April/Day-28/Cpp solution/soluition.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestSubstr(string& s, int k) {
+        vector<int> freq(26, 0);
+        int left = 0, maxFreq = 0, result = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            freq[s[right] - 'A']++;
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            // If more than k replacements needed, shrink window
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            result = max(result, right - left + 1);
+        }
+
+        return result;
+    }
+};
