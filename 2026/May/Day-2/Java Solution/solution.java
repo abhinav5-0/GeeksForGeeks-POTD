e74abaa74795ff80bb28340016b66335afbe66dd
Abhinav Kumar <133515688+abhinav5-0@users.noreply.github.com>
2026-05-02 23:41:31 +0530

    Create solution.java

---
+++ 2026/May/Day-2/Java Solution/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findPosition(int n) {
+        // Check if exactly one set bit exists
+        if (n <= 0 || (n & (n - 1)) != 0)
+            return -1;
+
+        int pos = 1;
+        while (n > 1) {
+            n = n >> 1;
+            pos++;
+        }
+        return pos;
+    }
+}
